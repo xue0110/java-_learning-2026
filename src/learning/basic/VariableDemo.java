6ff74daaf0e369aeae388a973d920f9d51c75a1a
XUE0110 <3277728425@qq.com>
2026-09-10 21:23:49 +0800

    feat: learn variables

---
+++ src/learning/basic/VariableDemo.java
@@ -0,0 +1,33 @@
+package learning.basic;
+
+public class VariableDemo {
+    public static void main(String[] args) {
+
+        // 整数
+        int age = 20;
+
+        // 小数
+        double height = 175.5;
+
+        // 字符
+        char gender = 'M';
+
+        // 布尔
+        boolean student = true;
+
+        // 字符串
+        String name = "XUE";
+
+        System.out.println("姓名：" + name);
+        System.out.println("年龄：" + age);
+        System.out.println("身高：" + height);
+        System.out.println("性别：" + gender);
+        System.out.println("是否学生：" + student);
+        //练习
+        int studentId = 123456;
+        double gpa = 3.2;
+        boolean hasComputer = true;
+        String game = "Sekiro";
+
+    }
+}
