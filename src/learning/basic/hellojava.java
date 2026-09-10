9711f4001c3cfd38ac4aa156bf9ebff3e1e5fa7b
XUE0110 <3277728425@qq.com>
2026-09-10 20:47:40 +0800

    feat: add first Java learning code

---
+++ src/learning/basic/hellojava.java
@@ -0,0 +1,17 @@
+package learning.basic;
+
+public class hellojava {
+    public static void main(String[] args) {
+
+    System.out.println("Hello Java!");
+    System.out.println("开始重新学习 Java");
+    System.out.println("2026 Java Learning");
+
+    //练习
+    System.out.println("newade");
+    System.out.println("daxue");
+    System.out.println("软件工程");
+
+
+}
+}
